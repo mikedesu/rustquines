6980687e83d145cabefb213451817a68ad377022
darkmage <165251+mikedesu@users.noreply.github.com>
2023-12-22 14:41:53 -0600

    updated the template so that we dont newline when inside double-quotes

--- makequine.py
+++ makequine.py
@@ -1,19 +1,51 @@
-filename="template.rs"
-f=open(filename,"r")
-content=f.read()
-f.close()
-s = "["
-for i in range(len(content)):
-    c = content[i]
-    v = ord(c)
-    s += f"{v}"
-    if i < len(content)-1:
-        s += ","
-s += "];"
-for c in content:
-    if c == "@":
-        print(s, end="")
-    elif c == "\n":
-        continue
-    else:
-        print(c, end="")
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python makequine.py <filename>")
+        return
+    filename=sys.argv[1]
+    try:
+        f=open(filename,"r")
+    except:
+        print("File not found")
+        return
+    content=f.read()
+    f.close()
+
+    content2 = ""
+    just_hit_newline = False
+    for c in content:
+        if c == "\n":
+            just_hit_newline = True
+            content2 += c
+        elif just_hit_newline and c == " ": # skip spaces after newline
+            continue
+        else:
+            just_hit_newline = False
+            content2 += c
+
+    content = content2
+
+    s = "["
+    for i in range(len(content)):
+        c = content[i]
+        v = ord(c)
+        s += f"{v}"
+        if i < len(content)-1:
+            s += ","
+    s += "];"
+
+
+    for c in content:
+        if c == "@":
+            print(s, end="")
+        #elif c == "\n":
+        #    continue
+        else:
+            print(c, end="")
+
+
+if __name__ == '__main__':
+    main()
+
--- template.rs
+++ template.rs
@@ -1,1 +1,50 @@
-fn main() {let v=@let mut i=0;let x=20;for c in v{if c==64{print!("[");for j in 0..v.len(){print!("{}",v[j]);if v[j]>100{i+=3;} else if v[j]>10{i+=2;} else{i+=1;}if i>x{print!("\n");i=0;}if j<v.len()-1{print!(",");i+=1;if i>x{print!("\n");i=0;}}}print!("];");}else{print!("{}",char::from_u32(c).unwrap());i+=1;if(c==59||c==123||c==32)&&i>x{print!("\n");i=0;}}}}
+fn main() {
+    let v=@
+    let mut i=0;
+    let x=20;
+    let mut is_inside_quotes=false;
+    for c in v{
+        if c==10{
+            continue;
+        }
+        else if c==64{
+            print!("[");
+            for j in 0..v.len(){
+                print!("{}",v[j]);
+                if v[j]>100{
+                    i+=3;
+                } else if v[j]>10{
+                    i+=2;
+                } else{
+                    i+=1;
+                }
+                if i>x{
+                    print!("\n");
+                    i=0;
+                }
+                if j<v.len()-1{
+                    print!(",");
+                    i+=1;
+                    if i>x{
+                        print!("\n");
+                        i=0;
+                    }
+                }
+            }
+            print!("];");
+        }
+        else{
+            print!("{}",char::from_u32(c).unwrap());
+            i+=1;
+            if(c==59||c==123||c==125||c==32)&&i>x{
+                if !is_inside_quotes{
+                    println!();
+                    i=0;
+                }
+            }
+            else if c==34{
+                is_inside_quotes=!is_inside_quotes;
+            }
+        }
+    }
+}
