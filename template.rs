250382fddd40818954847757c1187fb7e9e54e5b
darkmage <165251+mikedesu@users.noreply.github.com>
2023-12-21 19:39:50 -0600

    added templates that handle tower generation

--- template.rs
+++ template.rs
@@ -1,11 +1,1 @@
-fn main() {
-let v=@
-for c in v{
-if c==64{
-print!("{:?}",v);
-}
-else{
-print!("{}",char::from_u32(c).unwrap());
-}
-}
-}
+fn main() {let v=@let mut i=0;let x=20;for c in v{if c==64{print!("[");for j in 0..v.len(){print!("{}",v[j]);if v[j]>100{i+=3;} else if v[j]>10{i+=2;} else{i+=1;}if i>x{print!("\n");i=0;}if j<v.len()-1{print!(",");i+=1;if i>x{print!("\n");i=0;}}}print!("];");}else{print!("{}",char::from_u32(c).unwrap());i+=1;if(c==59||c==123||c==32)&&i>x{print!("\n");i=0;}}}}
