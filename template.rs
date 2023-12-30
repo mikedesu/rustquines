ea96b3ce0ef4625c60d30b1cb73251fbbe23ad0c
darkmage <165251+mikedesu@users.noreply.github.com>
2023-12-30 17:25:11 -0600

    updated template.rs to integrate interwoven lines as we work towards a symmetrical square

--- template.rs
+++ template.rs
@@ -1,19 +1,19 @@
-const COL_LEN : i32 = 100;
+const COL_LEN : i32 = 200;
 
-fn handle_spaces(num_spaces : i32) {
+fn handle_spaces(num_spaces:i32){
     for _ in 0..num_spaces{
         print!(" ");
     }
 }
 
-fn handle_newline_i_update(i : &mut i32, count : &mut i32) {
+fn handle_newline_i_update(i:&mut i32,count:&mut i32){
     if *i>=COL_LEN{
         *i=0;
         *count+=1;
     }
 }
 
-fn count_newlines(index : usize, v : &[u32]) -> i32{
+fn count_newlines(index:usize,v:&[u32])->i32{
     let mut count=0;
     let mut i=0;
     let mut is_inside_quotes=false;
@@ -36,7 +36,7 @@ fn count_newlines(index : usize, v : &[u32]) -> i32{
                     i+=1;
                     handle_newline_i_update(&mut i,&mut count);
                 }
-                last_num = v[j];
+                last_num=v[j];
             }
             if last_num>=100{
                 i+=3;
@@ -65,30 +65,55 @@ fn count_newlines(index : usize, v : &[u32]) -> i32{
 }
 
 
-fn handle_array_spacing(i : &mut i32, current_row : &mut i32, current_column : &mut i32, max_columns : i32, newline_ct : i32) {
-    const MAX_ROWS : i32 = 10;
+fn handle_array_spacing(i:&mut i32,current_row:&mut i32,current_column:&mut i32,max_columns:i32,newline_ct:i32){
+    const MAX_ROWS:i32=40;
     //if (*current_row < MAX_ROWS && *i>=COL_LEN)||(newline_ct<=MAX_ROWS&&*i>=COL_LEN){
-    if *current_row<MAX_ROWS&&*i>=COL_LEN{
+    if *current_column==1&&*i>=COL_LEN/4{
+        println!();
         *current_row+=1;
         *i=0;
+        *current_column=0;
     }
-    else if *current_row >= MAX_ROWS && *i>=COL_LEN/4{
-        if *current_column==0{
-            handle_spaces(COL_LEN/2 - (*i-(COL_LEN/4)));
-            *i=0;
-            *current_column+=1;
-        }
-        else {
-            println!();
-            *current_row+=1;
-            *current_column=0;
-            *i=0;
-        }
+    else if *current_row<MAX_ROWS&&*i>=COL_LEN{
+        println!();
+        *current_row+=1;
+        *i=0;
+        *current_column=0;
     }
+    else if *i>=COL_LEN{
+        println!();
+        *current_row+=1;
+        *i=0;
+        *current_column=0;
+    }
+    //else if *current_row >= MAX_ROWS*2 && *i>=COL_LEN{
+    //    println!();
+    //    *current_row+=1;
+    //    *i=0;
+    //    *current_column=0;
+    //}
+    else if *current_column==0&&*i>=COL_LEN/4&&*current_row%2==0{
+        handle_spaces(COL_LEN/2-(*i-(COL_LEN/4)));
+        *i=0;
+        *current_column+=1;
+    }
+    else if *current_column==1&&*i>=COL_LEN/4&&*current_row%2==0{
+        println!();
+        *current_row+=1;
+        *current_column=0;
+        *i=0;
+    }
+    else if *i>=COL_LEN&&*current_row%2==1{
+        println!();
+        *current_row+=1;
+        *current_column=0;
+        *i=0;
+    }
+
 }
 
 
-fn print_array(i : &mut i32, current_row : &mut i32, v : &[u32], current_column : &mut i32, max_columns : i32, newline_ct : i32) {
+fn print_array(i:&mut i32,current_row:&mut i32,v:&[u32],current_column:&mut i32,max_columns:i32,newline_ct:i32) {
     print!("[");
     *i+=1;
     handle_array_spacing(i,current_row,current_column,max_columns,newline_ct);       
@@ -118,16 +143,31 @@ fn print_array(i : &mut i32, current_row : &mut i32, v : &[u32], current_column 
 
 
 
-fn handle_special_char_spacing(i : &mut i32, current_row : &mut i32, current_column : &mut i32, max_columns : i32, is_inside_quotes : bool, newline_ct : i32) {
-    const MAX_ROWS : i32 = 10;
-    if *current_row < MAX_ROWS && *i>=COL_LEN{
+fn handle_special_char_spacing(i:&mut i32,current_row:&mut i32,current_column:&mut i32,max_columns:i32,is_inside_quotes:bool,newline_ct:i32){
+    const MAX_ROWS:i32=20;
+    if *current_row<MAX_ROWS&&*i>=COL_LEN{
         if !is_inside_quotes{
             println!();
             *current_row+=1;
             *i=0;
         }
     }
-    else if newline_ct<=MAX_ROWS {
+    else if *current_row>=MAX_ROWS&&*i>=COL_LEN/4&&newline_ct>=MAX_ROWS{
+        if !is_inside_quotes{
+            if *current_column==0{
+                handle_spaces(COL_LEN/2-(*i-(COL_LEN/4)));
+                *i=0;
+                *current_column+=1;
+            }
+            else {
+                println!();
+                *current_row+=1;
+                *current_column=0;
+                *i=0;
+            }
+        }
+    }
+    else if *current_column==1&&*i>=COL_LEN/4{
         if !is_inside_quotes{
             println!();
             *current_row+=1;
@@ -135,26 +175,32 @@ fn handle_special_char_spacing(i : &mut i32, current_row : &mut i32, current_col
             *i=0;
         }
     }
-    else if *current_row >= MAX_ROWS && *i>=COL_LEN/4{
-        if *current_column==0{
-            handle_spaces(COL_LEN/2 - (*i-(COL_LEN/4)));
-            *i=0;
-            *current_column+=1;
-        }
-        else {
+    else if *i>=COL_LEN {
+        if !is_inside_quotes{
             println!();
             *current_row+=1;
-            *current_column=0;
             *i=0;
         }
     }
 }
 
 
-fn handle_special_char(c : u32, i : &mut i32, current_row : &mut i32, is_inside_quotes : &mut bool, current_column : &mut i32, max_columns : i32, newline_ct : i32) {
+fn handle_special_char(c:u32,i:&mut i32,current_row:&mut i32,is_inside_quotes:&mut bool,current_column:&mut i32,max_columns:i32,newline_ct:i32){
     print!("{}",char::from_u32(c).unwrap());
     *i+=1;
-    if c==32||c==40||c==41||c==59||c==123||c==125{ 
+    // space
+    // open paren
+    // close paren
+    // asterisk
+    // comma
+    // colon
+    // semicolon
+    // open curly brace
+    // close curly brace
+    // open bracket
+    // close bracket
+    //if c==32||c==40||c==41||c==59||c==123||c==125{ 
+    if c==32||c==40||c==41||c==42||c==44||c==58||c==59||c==91||c==93||c==123||c==125{ 
         handle_special_char_spacing(i,current_row,current_column,max_columns,*is_inside_quotes,newline_ct);
     }
     else if c==34{
@@ -163,26 +209,26 @@ fn handle_special_char(c : u32, i : &mut i32, current_row : &mut i32, is_inside_
 }
 
 
-fn main() {
+fn main(){
     let v=@
-    let mut i : i32 = 0;
-    let mut current_row : i32 = 0;
+    let mut i:i32=0;
+    let mut current_row:i32=0;
     let mut is_inside_quotes=false;
-    let mut max_columns = 1;
-    let mut current_column = 0;
+    let mut max_columns=1;
+    let mut current_column=0;
     for k in 0..v.len(){
-        let c = v[k];
-        let newline_ct=count_newlines(k, &v);
+        let c=v[k];
+        let newline_ct=count_newlines(k,&v);
         // any newlines present in the source will be skipped
         if c==10{
             continue;
         }
         // handle the @ symbol
         else if c==64{
-            print_array(&mut i,&mut current_row,&v,&mut current_column, max_columns, newline_ct);
+            print_array(&mut i,&mut current_row,&v,&mut current_column,max_columns,newline_ct);
         }
         else{
-            handle_special_char(c,&mut i,&mut current_row, &mut is_inside_quotes, &mut current_column, max_columns, newline_ct);
+            handle_special_char(c,&mut i,&mut current_row,&mut is_inside_quotes,&mut current_column,max_columns,newline_ct);
         }
     }
 }
